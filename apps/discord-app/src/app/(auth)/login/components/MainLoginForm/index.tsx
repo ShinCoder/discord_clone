9c1b65963559b929408b9b605932c8fe10131ead
ShinCoder <shincoder.forwork@gmail.com>
2023-11-27 21:52:07 +0700

    refactor: reduce MainLoginForm component's content to form content only

--- apps/discord-app/src/app/(auth)/login/components/MainLoginForm/index.tsx
+++ apps/discord-app/src/app/(auth)/login/components/MainLoginForm/index.tsx
@@ -2,7 +2,6 @@
 
 import { useCallback, useMemo } from 'react';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
 import { useForm } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/joi';
@@ -10,13 +9,10 @@ import joi from 'joi';
 import { useMutation, useQuery } from '@tanstack/react-query';
 
 import ControlledInputText from '@components/ControlledInputText';
-import CustomButton from '@elements/CustomButton';
-import { publicRoutes } from '@constants';
 import { getMe, login } from '@services';
 import { useAppDispatch } from '@redux/hooks';
 import { setLoading } from '@redux/slices/statusSlice';
 import { setToken } from '@redux/slices/authSlice';
-import { ForgotPasswordLink, RegisterLink } from './elements';
 
 import { ILoginDto } from '@prj/types/api';
 
@@ -82,73 +78,29 @@ const MainLoginForm = () => {
   );
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center'
-        }}
-      >
-        <Typography
-          variant='h1'
-          color='text.primary'
-        >
-          Welcome back!
-        </Typography>
-        <Typography
-          variant='subtitle1'
-          color='text.secondary'
-        >
-          {"We're so excited to see you again!"}
-        </Typography>
-      </Box>
-      <Box
-        component='form'
-        onSubmit={handleLogin}
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          marginTop: theme.spacing(2.5)
-        }}
-      >
-        <ControlledInputText
-          control={control}
-          name='username'
-          label='Email or phone number'
-          isRequired
-        />
-        <ControlledInputText
-          control={control}
-          name='password'
-          label='Password'
-          isPassword
-          isRequired
-          sx={{ marginTop: theme.spacing(2.5) }}
-        />
-        <ForgotPasswordLink href={publicRoutes.forgotPassword}>
-          Forgot your password?
-        </ForgotPasswordLink>
-        <CustomButton
-          type='submit'
-          variant='contained'
-        >
-          Log In
-        </CustomButton>
-        <Box sx={{ marginTop: theme.spacing(0.5) }}>
-          <Typography
-            component='span'
-            sx={{
-              color: theme.dcPalette.text.grey,
-              fontSize: '0.875rem',
-              lineHeight: '1rem'
-            }}
-          >
-            Need an account?{' '}
-          </Typography>
-          <RegisterLink href={publicRoutes.register}>Register</RegisterLink>
-        </Box>
-      </Box>
+    <Box
+      component='form'
+      onSubmit={handleLogin}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: theme.spacing(2.5)
+      }}
+    >
+      <ControlledInputText
+        control={control}
+        name='username'
+        label='Email or phone number'
+        isRequired
+      />
+      <ControlledInputText
+        control={control}
+        name='password'
+        label='Password'
+        isPassword
+        isRequired
+        sx={{ marginTop: theme.spacing(2.5) }}
+      />
     </Box>
   );
 };
--- apps/discord-app/src/app/(auth)/login/page.tsx
+++ apps/discord-app/src/app/(auth)/login/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
 
 import VerticalSeperator from '@components/VerticalSeperator';
+import CustomButton from '@elements/CustomButton';
+import { publicRoutes } from '@constants';
 import MainLoginForm from './components/MainLoginForm';
 import QRSection from './components/QRSection';
 import QRCode from './components/QRCode';
+import { ForgotPasswordLink, RegisterLink } from './elements';
 
 const Login = () => {
   const theme = useTheme();
@@ -22,7 +26,51 @@ const Login = () => {
         backgroundColor: 'background.default'
       }}
     >
-      <MainLoginForm />
+      <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Typography
+            variant='h1'
+            color='text.primary'
+          >
+            Welcome back!
+          </Typography>
+          <Typography
+            variant='subtitle1'
+            color='text.secondary'
+          >
+            {"We're so excited to see you again!"}
+          </Typography>
+        </Box>
+        <MainLoginForm />
+        <ForgotPasswordLink href={publicRoutes.forgotPassword}>
+          Forgot your password?
+        </ForgotPasswordLink>
+        <CustomButton
+          type='submit'
+          variant='contained'
+        >
+          Log In
+        </CustomButton>
+        <Box sx={{ marginTop: theme.spacing(0.5) }}>
+          <Typography
+            component='span'
+            sx={{
+              color: theme.dcPalette.text.grey,
+              fontSize: '0.875rem',
+              lineHeight: '1rem'
+            }}
+          >
+            Need an account?{' '}
+          </Typography>
+          <RegisterLink href={publicRoutes.register}>Register</RegisterLink>
+        </Box>
+      </Box>
       <VerticalSeperator radius={theme.spacing(4)} />
       <QRSection>
         <QRCode />
